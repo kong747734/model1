21149f1e32bfe51cf8c52cda304da3b329118533
赵耿忠 <zhaogengzhong@lighthin.cn>
2019-05-20 17:58:04 +0800

    model12

---
+++ app/src/main/java/com/xfhl/wawa/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.xfhl.wawa.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.xfhl.wawa.model12.Model12Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void tiao(View v) {
+        Intent intent=new Intent(this, Model12Activity.class);
+        startActivity(intent);
+    }
+}
+
---
+++ model12/src/main/java/com/xfhl/wawa/model12/Model12Activity.java
@@ -0,0 +1,14 @@
+package com.xfhl.wawa.model12;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Model12Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model);
+    }
+}
